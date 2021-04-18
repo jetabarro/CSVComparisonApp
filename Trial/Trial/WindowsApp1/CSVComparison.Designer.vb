ec8103721fd6269f231b7810a7484c44a30507bd
Jethro Abarro <jethro.abarro@gmail.com>
2021-04-18 20:57:14 +0800

    Add files via upload

---
+++ Trial/Trial/WindowsApp1/CSVComparison.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class CSVComparison
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.OpenFile1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.OpenFile2 = New System.Windows.Forms.Button()
+        Me.File1 = New System.Windows.Forms.OpenFileDialog()
+        Me.File2 = New System.Windows.Forms.OpenFileDialog()
+        Me.Path1 = New System.Windows.Forms.TextBox()
+        Me.Path2 = New System.Windows.Forms.TextBox()
+        Me.DataFile1 = New System.Windows.Forms.DataGridView()
+        Me.DataFile2 = New System.Windows.Forms.DataGridView()
+        Me.Compare = New System.Windows.Forms.Button()
+        CType(Me.DataFile1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataFile2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'OpenFile1
+        '
+        Me.OpenFile1.Location = New System.Drawing.Point(496, 13)
+        Me.OpenFile1.Name = "OpenFile1"
+        Me.OpenFile1.Size = New System.Drawing.Size(75, 23)
+        Me.OpenFile1.TabIndex = 0
+        Me.OpenFile1.Text = "Open"
+        Me.OpenFile1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(24, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "File 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(784, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "File 2"
+        '
+        'OpenFile2
+        '
+        Me.OpenFile2.Location = New System.Drawing.Point(1256, 15)
+        Me.OpenFile2.Name = "OpenFile2"
+        Me.OpenFile2.Size = New System.Drawing.Size(75, 23)
+        Me.OpenFile2.TabIndex = 2
+        Me.OpenFile2.Text = "Open"
+        Me.OpenFile2.UseVisualStyleBackColor = True
+        '
+        'File1
+        '
+        Me.File1.FileName = "OpenFileDialog1"
+        '
+        'File2
+        '
+        Me.File2.FileName = "OpenFileDialog1"
+        '
+        'Path1
+        '
+        Me.Path1.Location = New System.Drawing.Point(72, 13)
+        Me.Path1.Name = "Path1"
+        Me.Path1.Size = New System.Drawing.Size(418, 22)
+        Me.Path1.TabIndex = 4
+        '
+        'Path2
+        '
+        Me.Path2.Location = New System.Drawing.Point(832, 16)
+        Me.Path2.Name = "Path2"
+        Me.Path2.Size = New System.Drawing.Size(418, 22)
+        Me.Path2.TabIndex = 5
+        '
+        'DataFile1
+        '
+        Me.DataFile1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataFile1.Location = New System.Drawing.Point(12, 42)
+        Me.DataFile1.Name = "DataFile1"
+        Me.DataFile1.RowHeadersWidth = 51
+        Me.DataFile1.RowTemplate.Height = 24
+        Me.DataFile1.Size = New System.Drawing.Size(685, 462)
+        Me.DataFile1.TabIndex = 6
+        '
+        'DataFile2
+        '
+        Me.DataFile2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataFile2.Location = New System.Drawing.Point(718, 43)
+        Me.DataFile2.Name = "DataFile2"
+        Me.DataFile2.RowHeadersWidth = 51
+        Me.DataFile2.RowTemplate.Height = 24
+        Me.DataFile2.Size = New System.Drawing.Size(709, 460)
+        Me.DataFile2.TabIndex = 7
+        '
+        'Compare
+        '
+        Me.Compare.Location = New System.Drawing.Point(1312, 512)
+        Me.Compare.Name = "Compare"
+        Me.Compare.Size = New System.Drawing.Size(95, 23)
+        Me.Compare.TabIndex = 8
+        Me.Compare.Text = "Compare"
+        Me.Compare.UseVisualStyleBackColor = True
+        '
+        'CSVComparison
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1439, 547)
+        Me.Controls.Add(Me.Compare)
+        Me.Controls.Add(Me.DataFile2)
+        Me.Controls.Add(Me.DataFile1)
+        Me.Controls.Add(Me.Path2)
+        Me.Controls.Add(Me.Path1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.OpenFile2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.OpenFile1)
+        Me.Name = "CSVComparison"
+        Me.Text = "Path1"
+        CType(Me.DataFile1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataFile2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents OpenFile1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents OpenFile2 As Button
+    Friend WithEvents File1 As OpenFileDialog
+    Friend WithEvents File2 As OpenFileDialog
+    Friend WithEvents Path1 As TextBox
+    Friend WithEvents Path2 As TextBox
+    Friend WithEvents DataFile1 As DataGridView
+    Friend WithEvents DataFile2 As DataGridView
+    Friend WithEvents Compare As Button
+End Class
---
+++ Trial/Trial/WindowsApp1/CSVComparison.vb
@@ -0,0 +1,121 @@
+﻿Imports System.IO
+Imports System.Text
+
+
+Public Class CSVComparison
+
+    Public DataTableFile1 As DataTable
+    Public DataTableFile2 As DataTable
+    Public DataTable1Diff As DataTable
+    Public DataTable2Diff As DataTable
+
+    Private Sub OpenFile1_Click(sender As Object, e As EventArgs) Handles OpenFile1.Click
+        File1.Filter = "CSV files (*.csv)|*.CSV"
+        File1.FilterIndex = 2
+        File1.RestoreDirectory = True
+        Dim result As DialogResult = File1.ShowDialog()
+
+        ' Test result.
+        If result = Windows.Forms.DialogResult.OK Then
+
+            ' Get the file name.
+            Dim path As String = File1.FileName
+            Try
+                ' For debugging.
+                Path1.Text = path
+
+                DataTableFile1 = Me.ConvertCSVToDataTable(path)
+
+                DataFile1.DataSource = DataTableFile1
+
+            Catch ex As Exception
+
+                ' Report an error.
+                Me.Text = "Error"
+
+            End Try
+        End If
+    End Sub
+
+    Private Sub OpenFile2_Click(sender As Object, e As EventArgs) Handles OpenFile2.Click
+        File2.Filter = "CSV files (*.csv)|*.CSV"
+        File2.FilterIndex = 2
+        File2.RestoreDirectory = True
+        Dim result As DialogResult = File2.ShowDialog()
+
+        ' Test result.
+        If result = Windows.Forms.DialogResult.OK Then
+
+            ' Get the file name.
+            Dim path As String = File2.FileName
+            Try
+                ' For debugging.
+                Path2.Text = path
+
+                DataTableFile2 = Me.ConvertCSVToDataTable(path)
+
+                DataFile2.DataSource = DataTableFile2
+
+            Catch ex As Exception
+
+                ' Report an error.
+                Me.Text = "Error"
+
+            End Try
+        End If
+    End Sub
+
+    Private Function ConvertCSVToDataTable(ByVal path As String) As DataTable
+        Dim dt As DataTable = New DataTable()
+        Dim dt2 As DataTable = New DataTable()
+        Using con As OleDb.OleDbConnection = New OleDb.OleDbConnection()
+            Try
+                con.ConnectionString = String.Format("Provider={0};Data Source={1};Extended Properties=""Text;HDR=NO;FMT=Delimited""", "Microsoft.Jet.OLEDB.4.0", IO.Path.GetDirectoryName(path))
+                Using cmd As OleDb.OleDbCommand = New OleDb.OleDbCommand("SELECT * FROM " & IO.Path.GetFileName(path), con)
+                    Using da As OleDb.OleDbDataAdapter = New OleDb.OleDbDataAdapter(cmd)
+                        con.Open()
+                        da.Fill(dt)
+                        con.Close()
+                    End Using
+                End Using
+            Catch ex As Exception
+                Console.WriteLine(ex.ToString())
+            Finally
+                If con IsNot Nothing AndAlso con.State = ConnectionState.Open Then
+                    con.Close()
+                End If
+            End Try
+        End Using
+        dt2 = dt.AsEnumerable().Skip(7).CopyToDataTable()
+        Return dt2
+    End Function
+
+    Private Sub Compare_Click(sender As Object, e As EventArgs) Handles Compare.Click
+
+        DataTable2Diff = DataTableFile2.Copy()
+        DataTable1Diff = DataTableFile1.Copy()
+
+        If DataTableFile1 IsNot Nothing And DataTableFile2 IsNot Nothing Then
+            Me.Text = "Comparing"
+            For rw1 As Integer = (DataTableFile1.Rows.Count - 1) To 0 Step -1
+
+                For rw2 As Integer = (DataTableFile2.Rows.Count - 1) To 0 Step -1
+                    If DataTableFile1.Rows(rw1).ItemArray.Length > 0 And DataTableFile2.Rows(rw2).ItemArray.Length > 0 Then
+                        Dim array1 As Object() = DataTableFile1.Rows(rw1).ItemArray
+                        Dim array2 As Object() = DataTableFile2.Rows(rw2).ItemArray
+
+                        If array1.SequenceEqual(array2) Then
+                            DataTable1Diff.Rows.Remove(DataTable1Diff.Rows(rw1))
+                            DataTable2Diff.Rows.Remove(DataTable2Diff.Rows(rw2))
+                        End If
+                    End If
+                Next
+
+            Next
+            DataFile2.DataSource = DataTable1Diff
+            DataFile1.DataSource = DataTable2Diff
+            Me.Text = "Done Comparing"
+        End If
+
+    End Sub
+End Class
